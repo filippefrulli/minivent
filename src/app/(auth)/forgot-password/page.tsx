b678ccd56a2a63720a18658c3c5419e60e9757db
Filippe Frulli <filippe.frulli@campus.lmu.de>
2024-09-22 22:13:22 +0100

    first commit

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ForgotPasswordPage = () => {
+  return (
+    <div>
+      <h1>Forgot password page</h1>
+    </div>
+  );
+}
+
+export default ForgotPasswordPage;
