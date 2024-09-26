31e86c5bb2c690f8e8795f6dd8ec62c2bbaef7b0
Filippe Frulli <filippe.frulli@campus.lmu.de>
2024-09-26 22:56:36 +0100

    added city search box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,49 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./home.module.css";
 
 const Home = () => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [text, setText] = useState("Chiavari");
+
+  const handleTextClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleBlur = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.searchContainer}>
-        <a className={styles.normalText}>Discover events near</a>
+        <div>
+          <a className={styles.normalText}>Discover events near</a>
+          {isEditing ? (
+            <input
+              type="text"
+              placeholder={text}
+              onChange={handleInputChange}
+              onBlur={handleBlur}
+              autoFocus
+              className={styles.largeTextInput}
+            />
+          ) : (
+            <a className={styles.largeText} onClick={handleTextClick}>
+              {text}
+            </a>
+          )}
+        </div>
       </div>
       <div className={styles.eventList}></div>
       <div className={styles.mapContainer}></div>
     </div>
   );
-}
+};
 
 export default Home;
